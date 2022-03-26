5a5ffd6a1679bf596658fc9d001869104b4a5fbb
lls <liulushi@xuetangx.com>
2022-03-26 22:02:42 +0800

    ‘首版提交’

---
+++ LoginViewController.h
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.h
+//  LSLoginLibary
+//
+//  Created by lls on 2022/3/26.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoginViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LoginViewController.m
@@ -0,0 +1,33 @@
+//
+//  LoginViewController.m
+//  LSLoginLibary
+//
+//  Created by lls on 2022/3/26.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    NSLog(@"LoginViewController");
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
